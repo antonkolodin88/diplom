9f94e15b48c96f22adbf52ee47e93dd40c059bac
antonkolodin88 <xwphoenix@gmail.com>
2025-05-20 23:50:11 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://cd420b62-36ec-4928-8c46-144683fe3259.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+FIND_ORDER_FROM_TRACK_PATH = "/api/v1/orders/track?t="
